63b74484a6b53926c1a106396bd9f2ac4c2d255e
John Carpenter <john.carpenter@blacksquare.io>
2023-04-12 16:28:17 -0600

    removing training for now

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,5 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
   { href: '/demo', title: 'Sample Agents' },
-  { href: '/training', title: 'Training' },  
 ]
 export default headerNavLinks
